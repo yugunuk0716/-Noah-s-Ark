d4aca04d7d32aa8d34a981f054ab691200942cbd
HanUseArch <wooyeophan@gmail.com>
2021-12-01 12:48:23 +0900

    적 채력

--- Noah's Ark/Assets/Scripts/AI/AIHealth.cs
+++ Noah's Ark/Assets/Scripts/AI/AIHealth.cs
@@ -1,8 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AIHealth : MonoBehaviour
 {
-    protected virtual int HP { get; set; }
+    /// <summary>
+    /// 채력 감소 시 호출<br/>사망 시 호출 안됨
+    /// </summary>
+    public event Action OnHealthDecreased;
+
+    /// <summary>
+    /// 사망 시 호출
+    /// </summary>
+    public event Action OnDead;
+
+    [SerializeField] private int _hp = 20;
+    public int HP { 
+        get {
+            return _hp;
+        }
+        set {
+            _hp = value;
+            switch(_hp <= 0)
+            {
+                case true: // 사망
+                    OnDead();
+                    break;
+                case false: // 채력 감소
+                    OnHealthDecreased();
+                    break;
+            }
+        } 
+    }
+
+
+    protected virtual void Awake()
+    {
+        OnHealthDecreased += () => { };
+        OnDead += Dead;
+    }
+
+    /// <summary>
+    /// 사망 시 호출됨
+    /// </summary>
+    protected virtual void Dead()
+    {
+        gameObject.SetActive(false);
+    }
 }
