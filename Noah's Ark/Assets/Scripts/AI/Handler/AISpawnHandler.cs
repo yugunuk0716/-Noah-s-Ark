55c82899a7efc9534955657e7202787c5588fdc1
HanUseArch <wooyeophan@gmail.com>
2021-12-13 12:37:44 +0900

    적 속도 따라 너프

--- Noah's Ark/Assets/Scripts/AI/Handler/AISpawnHandler.cs
+++ Noah's Ark/Assets/Scripts/AI/Handler/AISpawnHandler.cs
@@ -21,7 +21,7 @@ public class AISpawnHandler : MonoBehaviour, ISpawnable
     public void OnSpawn(GameObject enemy)
     {
         Debug.Log("OnSpawnEnemy");
-        health.HP = health.DefaultHP;
+        health.HP = health.DefaultHP + (int)aiBase.GetEnemyType() * 2;
         move.SetDefaultSpeed((int)anim.CurrentRunMode); // RunMode 에 른 속도
         anim.SetRunmode((AIAnimation.RunMode)aiBase.GetEnemyType());
         ActiveEnemyManager.Instance.OnSpawnEnemy -= OnSpawn;
